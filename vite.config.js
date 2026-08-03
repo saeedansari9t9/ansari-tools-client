d078185f53876848a5b42744e041af114c60f959
saeedansari9t9 <saeedansari9t9@gmail.com>
2026-08-03 20:30:50 +0500

    PWA Setup

--- vite.config.js
+++ vite.config.js
@@ -23,13 +23,13 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/favicon.png',
+            src: '/icons/icon-192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any maskable'
           },
           {
-            src: '/favicon.png',
+            src: '/icons/icon-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
